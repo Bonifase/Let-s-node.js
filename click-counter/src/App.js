b3211a27a63f86a848d5b024b466cd13bcd0125b
Bonifase <orwabonifase@gmail.com>
2018-11-08 11:58:08 +0300

    react fundermentals

---
+++ click-counter/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      This div has been clicked {this.props.clicks} times.
+      </div>
+    );
+  }
+}
+
+export default App;
