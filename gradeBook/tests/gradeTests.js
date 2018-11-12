d71fd59676e3e72225d707539f61ae34ec5c6258
Bonifase <orwabonifase@gmail.com>
2018-11-12 14:51:48 +0300

    testing node functiions

--- gradeBook/tests/gradeTests.js
+++ gradeBook/tests/gradeTests.js
@@ -4,16 +4,17 @@ exports["setUp"] = (callable) => {
     book.reset();
     callable();
 }
+
 exports["Can average grades"] = (test) => {
-    book.addGrade(90) 
-    book.addGrade(50)
+    book.addGrade(90); 
+    book.addGrade(50);
     var average = book.getAverage();
     test.equal(average, 70);
-    test.done() 
+    test.done() ;
 }
 exports["Can add new grade"] = (test) => {
-    book.addGrade(80)
+    book.addGrade(80);
     var count = book.getCountOfGrades();
-    test.equal(count, 1)
-    test.done()
+    test.equal(count, 1);
+    test.done();
 }
